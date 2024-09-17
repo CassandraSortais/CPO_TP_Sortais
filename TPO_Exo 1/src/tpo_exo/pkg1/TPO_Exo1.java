4ab42445e470b17e275241d21a0fb66c6e2dcbdb
Cassandra Sortais <cassandra.sortais@gmail.com>
2024-09-17 16:56:42 +0200

    Création d'un nouveau projet versionné

---
+++ TPO_Exo 1/src/tpo_exo/pkg1/TPO_Exo1.java
@@ -0,0 +1,31 @@
+/*
+*TP0 Exercice 1 
+*TDC - Groupe 1
+*SORTAIS Cassandra
+*Septembre 2024
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tpo_exo.pkg1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author virgi
+ */
+public class TPO_Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+// TODO code application logic here
+        Scanner sc;
+sc = new Scanner(System.in);
+System.out.println("Bonjour, quel est votre prenom ?");
+sc.nextLine();
+        
+    }
+    
+}

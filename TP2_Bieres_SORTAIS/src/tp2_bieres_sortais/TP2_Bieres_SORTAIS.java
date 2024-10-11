6d541d5a920e07ced692e88f63d7317265796375
Cassandra Sortais <cassandra.sortais@gmail.com>
2024-10-11 11:55:49 +0200

    correction : commit des fichiers caches de tous les projets

---
+++ TP2_Bieres_SORTAIS/src/tp2_bieres_sortais/TP2_Bieres_SORTAIS.java
@@ -0,0 +1,20 @@
+/*
+SORTAIS Cassandra
+TDC Groupe 1
+11 Octobre 2024
+package tp2_bieres_sortais;
+
+/**
+ *
+ * @author virgi
+ */
+public class TP2_Bieres_SORTAIS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

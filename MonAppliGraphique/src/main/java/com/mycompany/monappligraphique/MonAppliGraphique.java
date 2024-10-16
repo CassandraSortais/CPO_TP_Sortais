8760dd8fcbb045e5b9d017433c445a996a2a3e3a
cassandrasortais <cassandrasortais@localhost>
2024-10-16 12:31:19 +0200

    Création fenetre

---
+++ MonAppliGraphique/src/main/java/com/mycompany/monappligraphique/MonAppliGraphique.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.monappligraphique;
+
+/**
+ *
+ * @author cassandrasortais
+ */
+public class MonAppliGraphique {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        feunetre f = new feunetre();
+        f.show();
+        
+        feunetre f2 = new feunetre();
+        f2.show();
+    }
+}
